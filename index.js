360e154d83be9ca94193523bba1e3d6ac61576c1
Dominik <dlunko@foi.hr>
2022-06-27 19:10:31 +0200

    added folder structure

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors());
 // app.use('/movies', movieRoutes);
 // app.use('/users', userRoutes);
 
-// const CONNECTION_URL = 'mongodb://dominikLunko:dominikLunko123@cluster0-shard-00-00.6y2wy.mongodb.net:27017,cluster0-shard-00-01.6y2wy.mongodb.net:27017,cluster0-shard-00-02.6y2wy.mongodb.net:27017/baza?ssl=true&replicaSet=atlas-111r0u-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 
