2567fcae2ad175f1060fcf6394e8dff4475f76d6
Dominik <dlunko@foi.hr>
2022-09-14 21:09:05 +0200

    cookies fix

--- routes/user.js
+++ routes/user.js
@@ -9,13 +9,13 @@ const router = express.Router();
 router.post('/signin', signin);
 router.post('/signup', signup);
 router.get('/signout', signout);
-router.get('/refresh-token', refreshToken);
-router.put('/update-user', updateUser);
-router.post('/save-user-analytics', saveAnalytics)
-router.get('/get-user-analytics', getUserAnalytics)
-router.patch('/:nutrientId/add-to-favourite', addToFavourite);
-router.post('/daily-calory-intake', addToDailyCaloryIntake);
-router.post('/save-workout-plan', saveWorkoutPlan)
-router.delete('/delete-workout-plan/:workoutId', deleteWorkoutPlan)
+router.get('/refresh-token', auth, refreshToken);
+router.put('/update-user', auth, updateUser);
+router.post('/save-user-analytics', auth, saveAnalytics)
+router.get('/get-user-analytics', auth, getUserAnalytics)
+router.patch('/:nutrientId/add-to-favourite', auth, addToFavourite);
+router.post('/daily-calory-intake', auth, addToDailyCaloryIntake);
+router.post('/save-workout-plan', auth, saveWorkoutPlan)
+router.delete('/delete-workout-plan/:workoutId', auth, deleteWorkoutPlan)
 
 export default router;
